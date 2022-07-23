7c139d5a1dd28e1c45dc33fafdc40e4d85d8c605
Sarah Cartwright <sarah@untamed.zone>
2022-07-23 07:20:07 +0100

    Update loop_print.ino

--- examples/loop_print/loop_print.ino
+++ examples/loop_print/loop_print.ino
@@ -1,4 +1,4 @@
-#include<grove_terminal.h>
+#include <universal_terminal.h>
 #define TFT_WHITE   0xffff
 #define TFT_BLUE    0x001f
 
@@ -6,8 +6,6 @@ grove_terminal terminal;
 int32_t        value = 0;
 void setup() {
     terminal.begin();
-    terminal.global_foreground(TFT_WHITE);
-    terminal.global_background(TFT_BLUE);
 }
 
 void loop() {
