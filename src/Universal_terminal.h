69fe289bad1440b1f547bf36cb57fda17451919a
Sarah Cartwright <sarah@untamed.zone>
2022-07-29 14:16:53 +0100

    Update Universal_terminal.h

--- src/Universal_terminal.h
+++ src/Universal_terminal.h
@@ -19,14 +19,18 @@ typedef void (*Callback_Print)(const char str, uint16_t, uint16_t);
 typedef void (*Callback_ClearDisplay)(void); 
 
 class Universal_terminal {
-  public:	
-	//callbackPrint CPF;
+  public:
     void begin(Callback_Print, Callback_ClearDisplay, uint16_t columns, uint16_t rows);
     void serialDisplayBuffer(void);
     void displayRow(uint16_t row);
     void scrollUp(void);
     void scroll(void);
     void moveToNextColumn(void);
+    void clearRowBuffer(uint16_t row);
+    void clearRow(uint16_t row);
+    void setCursorRow(uint16_t row);
+    void setCursorColumn(uint16_t column);
+    void setCursor(uint16_t row, uint16_t column);
     gen_print(uint8_t);
     gen_print(uint16_t);
     gen_print(uint32_t);
